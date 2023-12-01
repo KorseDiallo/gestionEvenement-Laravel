e934e634b24c43984c851194e0d5361bdae4a9f5
KorseDiallo <abdoulayekorsediallo@gmail.com>
2023-12-01 17:07:39 +0000

    vue pour la partie client lamba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
@@ -117,9 +118,27 @@ body {
 }
 </style>
 <body>
-    <h1>page d'accueil</h1>
-  
-    <div class="card-list">
+    <nav class="navbar navbar-light bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white">Navbar</a>
+        
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              QUI ETES VOUS ??
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/accueilAssociation">Association??</a></li>
+              <li><a class="dropdown-item" href="/accueilClient">Client??</a></li>
+             
+            </ul>
+          </li>
+        </div>
+      </nav> 
+    <div class="container">
+        <img src="{{asset('image/logo.jpeg')}}" class="img-fluid mx-auto d-block" alt="Responsive image">
+    </div>
+   
+    <div class="card-list align-items-start">
         @foreach($evenements as $evenement)
         <a href="/accueilClient" class="card-item">
             @if($evenement->image_mise_en_avant)
@@ -135,6 +154,7 @@ body {
         </a>
         @endforeach
     </div>
-  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
